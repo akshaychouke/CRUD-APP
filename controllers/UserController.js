043b566fb26a2427fd5c1967235fb9fff78bcce1
Akshay Chouke <akshaychauke50@gmail.com>
2023-06-30 22:19:53 +0530

    Completed the DELETE opration

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,6 @@
 import User from "../model/UserSchema.js";
 
+//to add a new user to the database
 export const addUser = async (req, res) => {
   const user = req.body;
   const newUser = new User(user);
@@ -14,6 +15,7 @@ export const addUser = async (req, res) => {
   }
 };
 
+//to get all users from the database
 export const getAllUsers = async (req, res) => {
   try {
     const users = await User.find({});
@@ -25,6 +27,7 @@ export const getAllUsers = async (req, res) => {
   }
 };
 
+//to get a single user from the database
 export const getUser = async (req, res) => {
   try {
     const id = req.params.id;
@@ -36,15 +39,27 @@ export const getUser = async (req, res) => {
   }
 };
 
+//to edit a user in the database
 export const editUser = async (req, res) => {
   let user = req.body;
   const editUserData = new User(user);
   try {
-
-    await User.updateOne({_id: user._id}, editUserData);
+    await User.updateOne({ _id: user._id }, editUserData);
     res.status(200).json(`User ${editUserData.name} updated successfully`);
   } catch (error) {
     console.log("error in editUser controller", error.message);
     res.status(409).json({ message: error.message });
   }
-}
+};
+
+//to delete a user from the database
+export const deleteUser = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await User.deleteOne({ _id: id });
+    res.status(200).json(`User deleted successfully`);
+  } catch (error) {
+    console.log("error in deleteUser controller", error.message);
+    res.status(409).json({ message: error.message });
+  }
+};
--- routes/route.js
+++ routes/route.js
@@ -4,11 +4,13 @@ import {
   getAllUsers,
   getUser,
   editUser,
+  deleteUser,
 } from "../controllers/UserController.js";
 const router = express.Router();
 
 router.post("/add", addUser);
 router.get("/all", getAllUsers);
 router.get("/:id", getUser);
-router.post("/:id", editUser);
+router.put("/:id", editUser);
+router.delete("/:id", deleteUser);
 export default router;
